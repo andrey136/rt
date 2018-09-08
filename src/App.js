7449769923ec9fafed1d8a06826ae92e74224927
Andrey Mardash <andrey.mardash@gmail.com>
2018-09-08 18:44:07 +0300

    gh pages

--- src/App.js
+++ src/App.js
@@ -1,57 +1,94 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 
-class App extends Component{
-  constructor(props){
-      super(props);
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-      this.state = {
-          title: 'Counter: ',
-          counter: 10,
-          counters: [23, 45]
-      };
+        this.state = {
+            inputText: 'abc',
+            list: ['xfsfsa']
+        };
 
-  }
+    }
 
-  plus(){
-    const counter = this.state.counter + 1;
-    const counters = [...this.state.counters];
-    counters.push(counter);
-    this.setState({
-        counter,
-        counters
-    });
-  }
-
-  minus(){
-      const counter = this.state.counter - 1;
-      const counters = [...this.state.counters];
-      counters.push(counter);
-      this.setState({
-          counter,
-          counters
+    addToList() {
+      this.setState(
+        {list: [...this.state.list, this.state.inputText],
+        inputText: ''
       });
-  }
+    }
 
-  render() {
-    return (
-      <div className="App">
-          {this.state.title} {this.state.counter}
+    changeInput(value) {
+      this.setState({ inputText: value })
+    }
 
-      <hr/>
+    removeAllItems() {
+        this.setState({
+            list: [],
+            inputText: ''
+        })
+    }
 
-          <button onClick={() => this.plus()}>Plus</button>
-          <button onClick={() => this.minus()}>Minus</button>
+    loadTodo(){
+        console.log('Load');
+    }
 
-      <hr/>
+    render() {
+        return (
+            <div className="container">
+                <h1>TODO List</h1>
+                <button className="btn btn-primary" onClick={() => this.loadTodo()}>Load TODO</button>
+                <hr/>
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Text here"
+                        aria-label="Text"
+                        id="input"
+                        value={this.state.inputText}
+                        onChange={(e) => this.changeInput(e.target.value)}
+                    />
+                    <button
+                        className="btn btn-outline-secondary"
+                        onClick={() => this.addToList()}
+                    >Add to list
+                    </button>
+                    <button
+                        className="btn btn-success"
+                        onClick={() => this.removeAllItems()}
+                    >Remove all items
+                    </button>
+                </div>
 
-      <ul>
-          {this.state.counters.map(el => <li key ={Math.random()}>{el}</li>)}
-      </ul>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">My List</th>
+                            <th className="th">{}</th>
+                        </tr>
+                    </thead>
+                    <tdody id="list">{
+                        this.state.list.map((el, cnt) =>
+                            <tr>
+                                <td className="done">{el}</td>
+                                <td className="th">
+                                    <button className="btn btn-outline-success" key="0">Done</button>
+                                    <i key={cnt} className="fas fa-backspace">{}</i>
+                                </td>
+                            </tr>
+                        )
+                    }</tdody>
+                </table>
 
-      </div>
-    );
-  }
+                <ul>
+
+                </ul>
+
+            </div>
+        );
+    }
 }
 
 export default App;
