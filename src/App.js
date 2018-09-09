e95456f4a898ba57c0b3772fc6055b9451344da8
Andrey Mardash <andrey.mardash@gmail.com>
2018-09-09 19:06:56 +0300

    Edit Code

--- src/App.js
+++ src/App.js
@@ -1,133 +1,160 @@
 import React, {Component} from 'react';
 import uniqid from 'uniqid';
-import axios from 'axios';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            inputText: 'abc',
-            list: [{
-                title: 'Some',
-                id: uniqid(),
-                done: false,
-            }]
-        };
+    this.state = {
+      inputText: 'abc',
+      isEdited: false,
+      editedItem: {},
+      list: [{
+        title: 'Some',
+        id: uniqid(),
+        done: false
+      },{
+        title: 'Google',
+        id: uniqid(),
+        done: false
+      }]
+    };
+  }
 
-    }
+  addToList() {
+    const list = this.state.list;
+    list.push({
+      title: this.state.inputText,
+      id: uniqid(),
+      done: false
+    });
+    this.setState({
+      list: list,
+      inputText: ''
+    });
+  }
 
-    addToList() {
-      const list = this.state.list;
-      list.push({
-          title: this.state.inputText,
-          id: uniqid(),
-          done: false
-      });
-        this.setState({
-        list: list,
-        inputText: ''
-      });
-    }
+  changeInput(value) {
+    this.setState({inputText: value})
+  }
 
-    changeInput(value) {
-      this.setState({ inputText: value })
-    }
+  removeAllItems() {
+    this.setState({
+      list: [],
+      inputText: ''
+    })
+  }
 
-    removeAllItems() {
-        this.setState({
-            list: [],
-            inputText: ''
-        })
-    }
+  done(id) {
+    console.log('done');
+    const list = this.state.list;
+    list.map(el => el.id === id ? el.done = !el.done : '');
+    this.setState({
+      list: list
+    })
+  }
 
-    loadTodo(){
-        let list = this.state.list;
-        axios.get('https://jsonplaceholder.typicode.com/todos')
-            .then(function (response) {
-                let todos = response.data;
-                console.log(todos);
-                todos.map(el => ({
-                    ...el,
-                    done: el.completed
-                }));
-                list.concat(todos);
+  deleteItem(id) {
+    this.setState({
+      list: [...this.state.list.filter(el => el.id !== id)],
+    })
 
-            })
-            .catch(function (error) {
-                console.log('ERROR', error);
-            });
-        this.setState({
-            list: list
-        });
-    }
+  }
 
-    deleteItem(id){
-        this.setState({
-            list: [...this.state.list.filter(el => el.id !== id)],
-        })
+  editItem(el) {
+    console.log(el);
+    this.setState({ isEdited: true, editedItem: el });
+  }
 
-    }
+  changeEditedInput(value){
+    this.setState({
+      editedItem: {
+        ...this.state.editedItem,
+        title: value
+      }
+    })
+  }
 
-    done(id){
-        console.log('done');
-        const list = this.state.list;
-        list.map(el => el.id === id ? el.done = !el.done : '');
-        this.setState({
-            list: list
-        })
-    }
+  saveEditedItem() {
+    const id = this.state.editedItem.id;
+    const index = this.state.list.findIndex(el => el.id === id);
+    const newList = this.state.list;
+    newList[index] = this.state.editedItem;
 
-    render() {
-        return (
-            <div className="container">
-                <h1>TODO List</h1>
-                <button className="btn btn-primary" onClick={() => this.loadTodo()}>Load TODO</button>
-                <hr/>
-                <div className="input-group mb-3">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Text here"
-                        aria-label="Text"
-                        id="input"
-                        value={this.state.inputText}
-                        onChange={(e) => this.changeInput(e.target.value)}
-                    />
-                    <button
-                        className="btn btn-outline-secondary"
-                        onClick={() => this.addToList()}
-                    >Add to list
-                    </button>
-                    <button
-                        className="btn btn-success"
-                        onClick={() => this.removeAllItems()}
-                    >Remove all items
-                    </button>
-                </div>
+    this.setState({
+      list: newList,
+      editedItem: {},
+      isEdited: false
+    });
+    console.log(index);
+  }
 
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">My List</th>
-                            <th className="th">{}</th>
-                        </tr>
-                    </thead>
-                    <tbody id="list">{
-                        this.state.list.map((el) =>
-                            <tr key={el.id}>
-                                <td className={el.done + ""}>{el.id}{el.title}</td>
-                                <td className="th">
-                                    <button className="btn btn-outline-success" onClick={ () => this.done(el.id) }>Done</button>
-                                    <i className="fas fa-backspace" onClick={() => this.deleteItem(el.id) }>{}</i>
-                                </td>
-                            </tr>
-                        )
-                    }</tbody>
-                </table>
-            </div>
-        );
-    }
+  editForm() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.editedItem.title}
+          onChange={(e) => this.changeEditedInput(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={() => this.saveEditedItem()}>Save</button>
+      </div>
+    )
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="container">
+        <h1>TODO List</h1>
+        <hr/>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Text here"
+            aria-label="Text"
+            id="input"
+            value={this.state.inputText}
+            onChange={(e) => this.changeInput(e.target.value)}
+          />
+          <button
+            className="btn btn-outline-secondary"
+            onClick={() => this.addToList()}
+          >Add to list
+          </button>
+          <button
+            className="btn btn-success"
+            onClick={() => this.removeAllItems()}
+          >Remove all items
+          </button>
+        </div>
+
+        <table className="table">
+          <thead>
+          <tr>
+            <th scope="col">My List</th>
+            <th className="th">{}</th>
+          </tr>
+          </thead>
+          <tbody id="list">{
+            this.state.list.map((el) =>
+              <tr key={el.id}>
+                <td className={el.done + ""}>
+                  {el.title}
+                </td>
+                <td className="th">
+                  <button className="btn btn-outline-success" onClick={() => this.done(el.id)}>Done</button>
+                  <button className="btn btn-primary" onClick={() => this.editItem(el)}>Edit</button>
+                  <i className="fas fa-backspace" onClick={() => this.deleteItem(el.id)}>{}</i>
+                </td>
+              </tr>
+            )}</tbody>
+        </table>
+        {this.state.isEdited && this.editForm()}
+      </div>
+    );
+  }
 }
 
 export default App;
